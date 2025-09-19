f094892703893821555c842c6b14a8f84970cf67
ahmedghaoui46-oss <ghaouiahmed46@gmail.com>
2025-09-19 11:00:52 +0100

    save

--- Prototype/script.js
+++ Prototype/script.js
@@ -1,15 +1,17 @@
 function OccurenceNum() {
-    let list=["c1","c2","c1","c3","c4","c4","c1","c3","c1"];
+    let list = ["c1", "c2", "c1", "c3", "c4", "c4", "c1", "c3", "c1"];
 
-    let nameC=prompt("enter the c :");
-    let counter=0;
+    let nameC = prompt("enter the c :");
+    let counter = 0;
+
     for (let i = 0; i < list.length; i++) {
         if (nameC==list[i]) {
-            counter++
-      
+            counter++   
         
     }
     }
-    console.log("    the number of     "+nameC+" is "+ counter)
+
+    console.log("the number of " + nameC + " is " + counter);
 }
+
 OccurenceNum();
