9f48e6e82788bdfc00826bcb85a829ff8f45d056
ahmedghaoui46-oss <ghaouiahmed46@gmail.com>
2025-09-24 12:09:06 +0100

    save
    Signed-off-by: ahmedghaoui46-oss <ghaouiahmed46@gmail.com>

--- Prototype/script.js
+++ Prototype/script.js
@@ -2,6 +2,12 @@ function OccurenceNum() {
     let list = ["c1", "c2", "c1", "c3", "c4", "c4", "c1", "c3", "c1"];
 
     let nameC = prompt("Enter the code (ex: c1, c2...):");
+
+    if (!nameC) {
+        console.log("You did not enter a code.");
+        return;
+    }
+
     let counter = 0;
 
     for (let item of list) {
@@ -16,4 +22,5 @@ function OccurenceNum() {
         console.log(`${nameC} is not in the list`);
     }
 }
+
 OccurenceNum();
