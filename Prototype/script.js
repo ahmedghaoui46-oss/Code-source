da36afa2c688ac031e4f0678e54da50a35bc2d6a
ahmedghaoui46-oss <ghaouiahmed46@gmail.com>
2025-09-30 12:06:13 +0100

    save
    Signed-off-by: ahmedghaoui46-oss <ghaouiahmed46@gmail.com>

--- Prototype/script.js
+++ Prototype/script.js
@@ -1,15 +1,12 @@
 function OccurenceNum() {
     let list = ["c1", "c2", "c1", "c3", "c4", "c4", "c1", "c3", "c1"];
-
     let nameC = prompt("Enter the code (ex: c1, c2...):");
     let counter = 0;
-
     for (let item of list) {
         if (item === nameC) {
             counter++;
         }
     }
-
     if (counter > 0) {
         console.log(`The number of ${nameC} is ${counter}`);
     } else {
